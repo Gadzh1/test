6fb0ea19d119f1f53a6fbb487dc3366de3a66f2d
Gadzh1 <musievgadzi64@gmail.com>
2023-02-03 19:41:18 +0300

    reverse sentence func 2

--- reverse_sentence.py
+++ reverse_sentence.py
@@ -1,8 +1,22 @@
-def reverse_sentence(sen):
+def reverse_sentence(sen: str) -> str:
     rev_sen = ''
     for i in range(len(list(sen))):
         rev_sen += list(sen)[len(list(sen)) - 1 - i]
     return rev_sen
 
 
-print(reverse_sentence('asd'))
+def reverse_words(sen: str) -> str:
+    res = ''
+    for i in range(len(sen.split()) - 1, -1, -1):
+        res += sen.split()[i]
+        res += ' '
+    return res[:-1]
+
+
+def reverse_words_2(sen: str) -> str:
+    res = sen.split()
+    res.reverse()
+    return ' '.join(res)
+
+
+print(reverse_words_2('snow dog sun'))
